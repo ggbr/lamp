98df5dcdaccfded8fac8e8c059eb7c16809199d4
Gabriel <ggbr12.neg@gmail.com>
2020-09-09 11:29:34 -0300

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM php:7.2.2-apache
+FROM php:7.2.25-apache
 
 RUN apt-get upgrade -y
 RUN apt-get -y update --fix-missing
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('cpf')->unique();
+            $table->string('code');
+
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
